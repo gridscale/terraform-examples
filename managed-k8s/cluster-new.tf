ca90728626b380dc63b7c2df1714b7ee317d0763
Van Thong Nguyen <tom@gridscale.io>
2021-04-06 16:15:19 +0200

    change release_no to release

--- managed-k8s/cluster-new.tf
+++ managed-k8s/cluster-new.tf
@@ -9,8 +9,8 @@ terraform {
 
 resource "gridscale_k8s" "k8s-test" {
   name   = "test"
-  # Release number of k8s service. Define which release of k8s service will be used to create the k8s cluster. For convenience, please use gscloud to get the list of available k8s release numbers.
-  release_no = "1.18"
+  # K8s service release. Define which release of k8s service will be used to create the k8s cluster. For convenience, please use gscloud to get the list of available k8s release numbers.
+  release = "1.18"
   node_pool {
   name = "my_node_pool"
     node_count = 2
